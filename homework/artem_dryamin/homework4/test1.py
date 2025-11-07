dedfa94b2a49541bb0dd519039c71e3e1585f5a9
Avrelius71 <artem.dryamin_96@mail.ru>
2025-11-07 19:26:56 +0300

    homework lesson4 upadte3

--- homework/artem_dryamin/homework4/test1.py
+++ homework/artem_dryamin/homework4/test1.py
@@ -3,7 +3,7 @@ my_dict = {
     'list': [16, 17, 18, 15, 'denis'],
     'dict': {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5},
     'set': {'artem', 'yan', 4, 5, 7}
-    }
+          }
 print(my_dict['tuple'][-1])
 my_dict['list'].append(26)
 my_dict['list'].pop(1)
