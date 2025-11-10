a7a06008ae574416dd301620bc963ef000ecf0fd
Avrelius71 <artem.dryamin_96@mail.ru>
2025-11-11 00:17:42 +0300

    homework lesson5 new version

--- homework/artem_dryamin/homework5/test3.py
+++ homework/artem_dryamin/homework5/test3.py
@@ -1,3 +1,3 @@
 students = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
-print(f'Students {students[0]}, {students[1]}, {students[2]} study these subjects: {subjects[0]}, {subjects[1]}, {subjects[2]}')
+print(f'Students {", ".join(students)} study these subjects: {", ".join(subjects)}')
